50289e9cedc2afea45fae50d074c2d5dfeac9e8c
siltloam <tbrady1751@gmail.com>
2024-04-15 00:17:21 +0100

    Create basic structure for backend timer

--- py/timer/timer.py
+++ py/timer/timer.py
@@ -1,8 +1,25 @@
-class Timer():
-    
+#import time module
+import time
 
-    def __init__(self, initial_time):
-        self.initial_time = initial_time
+class Timer():
+    """Class for a Timer object. Tracks the time through an
+    initial time and a goal time.
+    """
     
-    # def setTimer(goalTime):
+    def __init__(self, set_timer):
+        """Constructor for Timer object.
+
+        Args:
+            set_timer (int): Time in seconds to add to timer
+        """
+        self.initial_time = time.time()
+        self.goal_time = self.initial_time + set_timer
+    
+    def seconds_left(self):
+        """Returns seconds left on timer.
+
+        Returns:
+            _type_: (int): Seconds left on timer
+        """
+        return self.goal_time - time.time()        
 
