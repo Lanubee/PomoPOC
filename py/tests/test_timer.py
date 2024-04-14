df5f83be4bd6fa883cdb8bbb8d8aa984bf51642e
siltloam <tbrady1751@gmail.com>
2024-04-15 00:52:14 +0100

    Create goal time test

--- py/tests/test_timer.py
+++ py/tests/test_timer.py
@@ -4,4 +4,9 @@ from timer.timer import Timer
 class TestTimer(unittest.TestCase):
 
     def setUp(self):
-        self.timer = Timer()
+        self.timer = Timer(5)
+
+    def test_set_time(self):
+        self.assertEqual(self.timer.goal_time, (self.timer.initial_time + 5))
+        
+    #def test_seconds_left():
