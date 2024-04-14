2a5f515985c9e1e0bf3e0aaa036019d1b725ecb1
siltloam <tbrady1751@gmail.com>
2024-04-14 23:50:47 +0100

    Add constructor and test folder for timer

---
+++ py/tests/test_timer.py
@@ -0,0 +1,7 @@
+import unittest
+from timer.timer import Timer
+
+class TestTimer(unittest.TestCase):
+
+    def setUp(self):
+        self.timer = Timer()
---
+++ py/timer/timer.py
@@ -0,0 +1,8 @@
+class Timer():
+    
+
+    def __init__(self, initial_time):
+        self.initial_time = initial_time
+    
+    # def setTimer(goalTime):
+
