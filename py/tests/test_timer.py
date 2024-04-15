a70458ac1ac3437b18f49dffe54baa90dfd8da46
Adam Ezzeldin <adamezzeldin2016@gmail.com>
2024-04-15 02:21:54 +0100

    Add pause and unpause methods. Add tests for pause and unpause methods

--- py/tests/test_timer.py
+++ py/tests/test_timer.py
@@ -14,3 +14,14 @@ class TestTimer(unittest.TestCase):
         time.sleep(1)
         self.assertTrue(self.timer.seconds_left() < self.timer.duration)
         
+    def test_pause_timer(self):
+        self.timer.pause_timer()
+        time.sleep(1)
+        self.assertEqual(self.timer.seconds_left(), 5)
+    
+    def test_unpause_timer(self):
+        self.timer.pause_timer()
+        time.sleep(1)
+        self.timer.unpause_timer()
+        time.sleep(1)
+        self.assertTrue(self.timer.seconds_left() < 4 and self.timer.seconds_left() > 3)
--- py/timer/timer.py
+++ py/timer/timer.py
@@ -5,7 +5,8 @@ class Timer():
     """Class for a Timer object. Tracks the time through an
     initial time and a goal time.
     """
-    
+    pause_pointer = -1
+
     def __init__(self, set_timer):
         """Constructor for Timer object.
 
@@ -15,6 +16,8 @@ class Timer():
         self.initial_time = time.time()
         self.goal_time = self.initial_time + set_timer
         self.duration = set_timer
+        self.time_paused = 0
+        self.paused = False
 
     def seconds_left(self):
         """Returns seconds left on timer.
@@ -22,5 +25,28 @@ class Timer():
         Returns:
             _type_: (int): Seconds left on timer
         """
-        return self.goal_time - time.time()        
+        if self.paused:
+            return self.duration
+        
+        return self.goal_time - time.time()     
+
+    def pause_timer(self):
+        """Pauses timer
+        """
+        self.duration = self.seconds_left()
+        Timer.pause_pointer = time.time()
+        self.paused = True
+
+        #TODO: Pause the timer display variable
+    
+    def unpause_timer(self):
+        """Unpauses timer, sets new goal_time and stores time elapsed while paused
+        """
+        self.goal_time = time.time() + self.duration
+        self.time_paused = time.time() - Timer.pause_pointer
+        self.paused = False
+
+        #TODO: Unpause timer display variable
+
+
 
