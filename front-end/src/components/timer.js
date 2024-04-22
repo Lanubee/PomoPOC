5185946de8a41086ec7820ebfccca047516b2f83
Adam Ezzeldin <adamezzeldin2016@gmail.com>
2024-04-22 20:10:44 +0100

    Add timer improved functionality with correct formatting

--- front-end/src/components/timer.js
+++ front-end/src/components/timer.js
@@ -2,17 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 function Timer() {
   const [timer, setTimer] = useState('00:00');
-  const [inputValue, setInputValue] = useState('');
+  const [inputValueSeconds, setInputValueSeconds] = useState('');
+  const [inputValueMinutes, setInputValueMinutes] = useState('');
 
   /**
    * Updates the timer to go down by 1 per second
    */
   useEffect(() => {
     let timerId;
-    if (timer !== null && timer > 0) {
-      timerId = setTimeout(() => setTimer(timer - 1), 1000);
-    } else {
-      setTimer('00:00');
+    if (timer !== '00:00') 
+    {
+      timerId = setTimeout(() => 
+      {
+      const timerParts = timer.split(':');
+      const minutes = parseInt(timerParts[0]);
+      const seconds = parseInt(timerParts[1]);
+      let newMinutes = minutes;
+      let newSeconds = seconds - 1;
+      if (newSeconds < 0)
+      {
+        newMinutes -= 1;
+        newSeconds = 59;
+      }
+      const minutesString = String(newMinutes).padStart(2, '0');
+      const secondsString = String(newSeconds).padStart(2, '0');
+      setTimer(minutesString + ':' + secondsString);
+    }, 1000);
     }
     return () => clearTimeout(timerId);
   }, [timer]);
@@ -22,19 +37,31 @@ function Timer() {
    * Checks if input is a number and larger than 0, sets timer if passes
    */
   const handleSetTimer = () => {
-    const timeInputValue = parseInt(inputValue, 10);
-    if (!isNaN(timeInputValue) && timeInputValue > 0) {
-      setTimer(timeInputValue);
+    const seconds = parseInt(inputValueSeconds, 10);
+    const minutes = parseInt(inputValueMinutes, 10);
+    if (!isNaN(minutes) && !isNaN(seconds) && minutes >= 0 && seconds >= 0) {
+      const minutesString = String(inputValueMinutes).padStart(2, '0')
+      const secondsString = String(inputValueSeconds).padStart(2, '0')
+      setTimer(minutesString + ':' + secondsString);
     } else {
       console.error('Invalid input for timer');
     }
   };
 
   /**
-   * Sets the inputValue from the input box 
+   * Sets the inputValueMinutes from minutes input box
+   * @param {*} event 
    */
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  const handleInputChangeMinutes = (event) => {
+    setInputValueMinutes(event.target.value);
+  }
+
+  /**
+   * Sets the inputValueSeconds from the seconds input box 
+   * @param {*} event
+   */
+  const handleInputChangeSeconds = (event) => {
+    setInputValueSeconds(event.target.value);
   };
 
   /**
@@ -49,9 +76,15 @@ function Timer() {
           <label htmlFor='timeInput'>Set Timer:</label>
           <input
             type="number"
-            id='timeInput'
-            value={inputValue}
-            onChange={handleInputChange}
+            id='timeInputMinutes'
+            value={inputValueMinutes}
+            onChange={handleInputChangeMinutes}
+          />
+          <input
+            type="number"
+            id='timeInputSeconds'
+            value={inputValueSeconds}
+            onChange={handleInputChangeSeconds}
           />
           <button onClick={handleSetTimer}>Submit</button>
         </div>
